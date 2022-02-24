e4e94d2212c8af56a95c08706d9b44dd61514ed1
cemir <cememir2017@gmail.com>
2022-02-24 21:38:03 +0300

    fonksiyonlar

---
+++ 2__functions.py
@@ -0,0 +1,20 @@
+def yaz():
+    print("yazdım")
+
+
+def topla(a, b):
+    print(a + b)
+
+def cikart(a, b):
+    print(a - b)
+
+
+print(yaz())
+
+# 24.02.2022 21:32
+print(topla(8, 8))
+print(cikart(8, 8))
+print(topla("cem", "emir"))
+print(topla("8", "8"))
+# print(topla("8", 8))
+# print(topla(8, "8"))
